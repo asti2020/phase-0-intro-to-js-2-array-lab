d983c93f3118823be52d1cb4664b60abdcce7a81
Astika Luintel <astild2020@gmail.com>
2022-08-19 00:49:11 -0400

    commit tree a21531bb52cf48848958e153e0adabd2c5ba364f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+ console.log(cats.push("Ralph"));
+}
+
+function destructivelyPrependCat(name){
+    console.log(cats.unshift("Bob"));
+}
+
+function destructivelyRemoveLastCat(){
+    console.log(cats.pop());
+}
+
+function destructivelyRemoveFirstCat(){
+    console.log(cats.shift());
+}
+
+function appendCat(name){
+ var newCats = cats.slice();
+ newCats.push("Broom");
+return newCats;
+}
+
+function  prependCat(name){
+    var borCats = ["Arnold", ...cats];
+    return borCats;
+};
+function removeLastCat(){
+    var nowCats = cats.slice();
+    nowCats.pop();
+    return nowCats;
+}
+
+function removeFirstCat(){
+    var byCats = cats.slice();
+    byCats.shift();
+    return byCats;
+}
